54e68b4fa00d5885ffec2c8e9f30f83d1176744f
Sun Xinyu <sunxinyu347@gmail.com>
2024-08-02 01:24:09 +0800

    use lambda for parse_einsum_string

--- einsum.hpp
+++ einsum.hpp
@@ -1,89 +1,61 @@
-#pragma once
-// Eigen head files
+#ifndef EINSUM_HPP
+#define EINSUM_HPP
+// Eigen3
 #define EIGEN_USE_THREADS
 #include <Eigen/Core>
 #include <unsupported/Eigen/CXX11/Tensor>
 #include <unsupported/Eigen/CXX11/ThreadPool>
-// fmt library
-#include "fmt/base.h"
+// fmt
 #include <fmt/core.h>
 #include <fmt/format.h>
 // C++ std
+#include <algorithm>
+#include <cmath>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
-// some useful function
+// main functions
+template <typename TensorType>
+bool tensor_equal(const TensorType &tensor1, const TensorType &tensor2,
+                  const double tol = 1e-10);
 template <typename TensorType> void print_tensor(const TensorType &tensor);
-
 template <int num_contractions, typename TensorType, int Dim1, int Dim2,
           int ResultDim>
 Eigen::Tensor<TensorType, ResultDim>
 einsum(const std::string &einsum_str,
        const Eigen::Tensor<TensorType, Dim1> &input1,
        const Eigen::Tensor<TensorType, Dim2> &input2);
-template <typename T>
-bool tensor_equal(const T &tensor1, const T &tensor2, const double tol = 1e-10);
+
+// Print formatted values
 /**
- * 判断两个张量（tensor）是否相等
+ * 打印格式化的值。
  *
- * @param tensor1 第一个张量
- * @param tensor2 第二个张量
- * @param tol 容差，用于判断维度大小的差异。默认为 1e-10
- * @return 如果张量的维度相同，并且每个维度的大小差异都在容差范围内，则返回
- * true，否则返回 false
- */
-template <typename TensorType>
-bool tensor_equal(const TensorType &tensor1, const TensorType &tensor2,
-                  const double tol) {
-  // Check if the dimensions are the same
-  if (tensor1.dimensions() != tensor2.dimensions()) {
-    return false;
-  }
-  // Compare each element with the given tolerance
-  for (auto i = 0; i < tensor1.size(); ++i) {
-    if (std::abs(tensor1.data()[i] - tensor2.data()[i]) > tol) {
-      return false;
-    }
-  }
-  return true;
-}
-/**
- * 打印格式化
+ * 这个模板函数根据传入的值的类型进行格式化打印。如果是整数类型，它将打印一个宽度为
+ * 12 的零填充整数。如果是浮点类型，它将打印一个宽度为 0，精度为 10
+ * 的浮点数格式。
  *
- * 这个模板函数 print_formatted
- * 用于打印不同类型的值，包括整型和浮点型。对于整型，它将打印一个以零填充的数字。
- * 对于浮点型，它将根据数字是否为零来采取不同的打印策略。如果数字为零，它将使用精度为
- * 8 的格式化字符串来打印一个以零填充的浮点数。
- * 如果数字不为零，它将使用精度为 8 的格式化字符串来打印浮点数，并显示 8
- * 位小数。
- *
- * @tparam T 要打印的值的类型
- * @param value 要打印的值
+ * @tparam T 要打印的值的类型，可以是整数类型或浮点类型。
+ * @param value 要打印的值。
  */
 template <typename T> void print_formatted(const T &value) {
   if constexpr (std::is_integral_v<T>) {
     fmt::print("{:12}", value); // Zero-padded integers
   } else if constexpr (std::is_floating_point_v<T>) {
-
     fmt::print("{:0.10f}", value);
   }
 }
 
+// Recursive tensor printing
 /**
- * 递归打印给定张量（Tensor）的元素，显示其形状结构
+ * 递归打印张量的内容
  *
- * 此函数用于递归地打印出给定张量的所有元素，以及它们在多维空间中的位置索引。它以一种人类可读的方式显示张量的结构，
- * 对每个元素，它会打印出其索引和值。对于多维张量，它会用方括号表示维度，并在每个维度级别上递归，直到打印完所有元素。
- *
- * @param tensor 要打印的张量对象，类型为 TensorType
- * @param shape 表示张量形状的尺寸向量，类型为 typename TensorType::Dimensions
- * @param dim 表示当前递归访问的维度索引，从
- * 0开始，表示最外层维度，逐渐向内递归
- * @param indices 一个大小为 dim
- * 的索引向量，表示当前访问的元素在每个维度上的索引位置
- * @param indent
- * 表示当前递归层级的缩进字符串，用于显示层次感，随着递归的深入，每一级都比上一级多两个空格
- * @param is_last 一个布尔值，表示当前元素是否是所在维度的最后一个元素
+ * @param tensor 要打印的张量对象
+ * @param shape 张量的形状，一个整数向量，表示每个维度的大小
+ * @param dim 当前递归到的维度
+ * @param indices 当前维度的索引向量
+ * @param indent 缩进字符串，用于表示递归层次
+ * @param is_last 是否是当前维度的最后一个元素
  */
 template <typename TensorType>
 void print_recursive(const TensorType &tensor,
@@ -92,20 +64,18 @@ void print_recursive(const TensorType &tensor,
                      bool is_last) {
   if (dim == shape.size()) {
     Eigen::array<Eigen::Index, TensorType::NumDimensions> eigen_indices;
-    for (int i = 0; i < TensorType::NumDimensions; ++i) {
-      eigen_indices[i] = static_cast<Eigen::Index>(indices[i]);
-    }
+    std::copy(indices.begin(), indices.end(), eigen_indices.begin());
     print_formatted(tensor(eigen_indices));
   } else {
     fmt::print("[");
     std::string new_indent = indent + " ";
     for (size_t i = 0; i < shape[dim]; ++i) {
-      indices.push_back(i);
+      indices.emplace_back(i);
       print_recursive(tensor, shape, dim + 1, indices, new_indent,
                       i == shape[dim] - 1);
       indices.pop_back();
       if (i < shape[dim] - 1) {
-        fmt::print(" "); // Space between elements in the same dimension
+        fmt::print(" ");
       }
     }
     fmt::print("]");
@@ -116,10 +86,12 @@ void print_recursive(const TensorType &tensor,
 }
 
 /**
- * @brief 打印张量的函数
+ * 打印张量的内容
  *
- * @tparam TensorType 模板参数，表示张量的类型
- * @param tensor 需要打印的张量
+ * 这个模板函数递归地打印张量的内容。它首先打印一个空格，然后调用`print_recursive`函数来打印张量的内容。`print_recursive`函数根据张量的维度和当前维度的索引，递归地打印张量中的每个元素。最后，`print_tensor`函数在打印完张量内容后，打印一个换行符。
+ *
+ * @tparam TensorType 张量的类型，需要支持.dimensions()成员函数来获取维度信息
+ * @param tensor 要打印的张量对象
  */
 template <typename TensorType> void print_tensor(const TensorType &tensor) {
   const auto &shape = tensor.dimensions();
@@ -128,55 +100,57 @@ template <typename TensorType> void print_tensor(const TensorType &tensor) {
   fmt::print("\n");
 }
 
-std::string find_different_element(const std::string &str1,
-                                   const std::string &str2,
-                                   std::vector<size_t> &left,
-                                   std::vector<size_t> &right) {
-
-  std::vector<char> set_I(str1.begin(), str1.end());
-  std::vector<char> set_D(str2.begin(), str2.end());
-
-  std::vector<char> difference;
-  // 找到str1中不在str2中的字符，并记录它们的索引
-  for (size_t i = 0; i < str1.size(); ++i) {
-    char c = str1[i];
-    if (str2.find(c) == std::string::npos) {
-      difference.push_back(c);
-      left.push_back(i); // 记录索引到left中
+// Check if tensors are equal
+/**
+ * 检查两个张量是否相等
+ *
+ * 这个函数将检查两个张量的维度是否匹配，以及它们的数据是否在给定的容差范围内相等。如果维度不匹配，或者任何对应位置的数据点差异大于容差，函数将返回
+ * false；如果所有数据点的差异都在容差范围内，函数将返回 true。
+ *
+ * @tparam TensorType
+ * 张量的类型，需要支持.dimensions()成员函数来获取维度信息，以及.data()成员函数来获取数据指针
+ * @param tensor1 第一个要比较的张量对象
+ * @param tensor2 第二个要比较的张量对象
+ * @param tol 容差，用来确定两个元素是否被认为是相等的。默认值为 1e-10
+ * @return 如果两个张量的所有元素在容差范围内相等，则为 true；否则为 false
+ */
+template <typename TensorType>
+bool tensor_equal(const TensorType &tensor1, const TensorType &tensor2,
+                  const double tol) {
+  if (tensor1.dimensions() != tensor2.dimensions()) {
+    return false;
+  }
+  for (auto i = 0; i < tensor1.size(); ++i) {
+    if (std::abs(tensor1.data()[i] - tensor2.data()[i]) > tol) {
+      return false;
     }
   }
-
-  // 找到str2中不在str1中的字符，并记录它们的索引
-  for (size_t i = 0; i < str2.size(); ++i) {
-    char c = str2[i];
-    if (str1.find(c) == std::string::npos) {
-      difference.push_back(c);
-      right.push_back(i); // 记录索引到right中
-    }
-  }
-
-  // 将不同元素组合成一个字符串
-  std::string different_elements(difference.begin(), difference.end());
-  return different_elements;
+  return true;
 }
 
-std::vector<size_t> find_indices(const std::string &result_indices,
-                                 const std::string &different_elements) {
-  std::vector<size_t> indices;
-  if (result_indices == different_elements) {
-    return indices;
-  }
-
-  for (char c : different_elements) {
-    auto pos = result_indices.find(c);
-    if (pos != std::string::npos) {
-      indices.push_back(pos);
-    }
-  }
-
-  return indices;
-}
-
+// Parse einsum string and perform contractions
+/**
+ * 解析爱因斯坦求和约定字符串
+ *
+ * 这个函数解析一个爱因斯坦求和约定字符串，并将结果存储在几个向量和一个字符串中，这些字符串和向量后来被用于进一步的计算。
+ *
+ * @param einsum_str 要解析的爱因斯坦求和约定字符串。这个字符串应该包含箭头 "->"
+ * 来分隔输入部分和输出部分。
+ * @param result_indices
+ * 输出字符串，包含爱因斯坦求和约定字符串中的索引，这些索引确定输出张量的维度。
+ * @param shuffle_indexs
+ * 索引的向量，这些索引表明结果应该如何在不同维度上进行重新排列。
+ * @param left
+ * 索引的向量，这些索引指向输入字符串的差异元素在左边字符串中的位置。
+ * @param right
+ * 索引的向量，这些索引指向输入字符串的差异元素在右边字符串中的位置。
+ * @return 一个
+ * std::vector<Eigen::IndexPair<int>>，其中每个元素都是一个包含两个索引的对；这些索引对对应于解析字符串时找到的匹配索引。
+ * @throws std::invalid_argument 如果爱因斯坦求和约定字符串格式不正确，例如缺少
+ * "->"，或者如果左边和右边字符串的长度不同，或者如果找不到输出索引，函数将抛出
+ * std::invalid_argument 异常。
+ *
+ */
 template <typename TensorType>
 std::vector<Eigen::IndexPair<int>>
 parse_einsum_string(const std::string &einsum_str, std::string &result_indices,
@@ -184,32 +158,27 @@ parse_einsum_string(const std::string &einsum_str, std::string &result_indices,
                     std::vector<size_t> &left, std::vector<size_t> &right) {
   std::vector<Eigen::IndexPair<int>> idx_pairs;
 
-  // Find "->" position
   auto arrow_pos = einsum_str.find("->");
   if (arrow_pos == std::string::npos) {
     throw std::invalid_argument("Invalid einsum string format: missing '->'");
   }
-  // Extract left and right parts
   auto left_part = einsum_str.substr(0, arrow_pos);
   result_indices = einsum_str.substr(arrow_pos + 2);
 
-  // Check if result_indices is empty after "->"
   if (result_indices.empty()) {
     throw std::invalid_argument(
         "Unsupport this code syntax, please set an index for output");
   }
 
-  // Split left part by commas
   std::vector<std::string> input_parts;
   auto comma_pos = left_part.find(',');
   while (comma_pos != std::string::npos) {
-    input_parts.push_back(left_part.substr(0, comma_pos));
+    input_parts.emplace_back(left_part.substr(0, comma_pos));
     left_part = left_part.substr(comma_pos + 1);
     comma_pos = left_part.find(',');
   }
-  input_parts.push_back(left_part);
+  input_parts.emplace_back(left_part);
 
-  // Check if exactly two input tensors are present
   if (input_parts.size() != 2) {
     throw std::invalid_argument(
         "Invalid number of input tensors in einsum string");
@@ -218,7 +187,47 @@ parse_einsum_string(const std::string &einsum_str, std::string &result_indices,
   const auto &I_indices = input_parts[0];
   const auto &D_indices = input_parts[1];
 
-  // Generate index pairs
+  auto find_different_element =
+      [](const std::string &str1, const std::string &str2,
+         std::vector<size_t> &left, std::vector<size_t> &right) -> std::string {
+    std::vector<char> difference;
+    for (size_t i = 0; i < str1.size(); ++i) {
+      char c = str1[i];
+      if (str2.find(c) == std::string::npos) {
+        difference.emplace_back(c);
+        left.emplace_back(i);
+      }
+    }
+    for (size_t i = 0; i < str2.size(); ++i) {
+      char c = str2[i];
+      if (str1.find(c) == std::string::npos) {
+        difference.emplace_back(c);
+        right.emplace_back(i);
+      }
+    }
+    return std::string(difference.begin(), difference.end());
+  };
+
+  auto find_indices =
+      [](const std::string &result_indices,
+         const std::string &different_elements) -> std::vector<size_t> {
+    std::vector<size_t> indices;
+    if (result_indices == different_elements) {
+      return indices;
+    }
+    for (char c : different_elements) {
+      auto pos = result_indices.find(c);
+      if (pos != std::string::npos) {
+        indices.emplace_back(pos);
+      }
+    }
+    return indices;
+  };
+
+  auto different_elements =
+      find_different_element(I_indices, D_indices, left, right);
+  shuffle_indexs = find_indices(result_indices, different_elements);
+
   for (int i = 0; i < I_indices.size(); ++i) {
     for (int j = 0; j < D_indices.size(); ++j) {
       if (I_indices[i] == D_indices[j]) {
@@ -226,13 +235,25 @@ parse_einsum_string(const std::string &einsum_str, std::string &result_indices,
       }
     }
   }
-  auto different_elements =
-      find_different_element(I_indices, D_indices, left, right);
-  shuffle_indexs = find_indices(result_indices, different_elements);
 
   return idx_pairs;
 }
 
+/**
+ * 使用爱因斯坦求和约定（Einstein Summation
+ * Convention）执行张量收缩操作，并在需要时对结果进行重排序。
+ *
+ * @tparam TensorType 表示张量的数据类型。
+ * @tparam ResultDim 表示结果张量的维度。
+ * @param einsum_str 一个字符串，指定爱因斯坦求和约定表示的索引操作。
+ * @param input1 第一个输入张量。
+ * @param input2 第二个输入张量。
+ * @return 收缩操作的结果作为一个 Eigen::Tensor<TensorType, ResultDim>
+ * 类型的张量。
+ * @throws std::invalid_argument 如果 einsum_str
+ * 格式不正确，或者输入张量的维度与 einsum_str
+ * 指定的索引不匹配，或者收缩维度错误。
+ */
 template <int num_contractions, typename TensorType, int Dim1, int Dim2,
           int ResultDim>
 Eigen::Tensor<TensorType, ResultDim>
@@ -241,7 +262,6 @@ einsum(const std::string &einsum_str,
        const Eigen::Tensor<TensorType, Dim2> &input2) {
   std::vector<size_t> left_idx;
   std::vector<size_t> right_idx;
-
   std::vector<size_t> shuffle_idx;
   std::string result_indices;
 
@@ -260,27 +280,26 @@ einsum(const std::string &einsum_str,
   if (ResultDim != left_idx.size() + right_idx.size()) {
     throw std::invalid_argument("Invalid number of dimensions in result");
   }
-  for (size_t i = 0; i < left_idx.size(); ++i) {
-    result_dimensions[i] = input1.dimension(left_idx[i]);
-  }
 
-  for (size_t i = 0; i < right_idx.size(); ++i) {
-    result_dimensions[left_idx.size() + i] = input2.dimension(right_idx[i]);
-  }
+  std::transform(left_idx.begin(), left_idx.end(), result_dimensions.begin(),
+                 [&](size_t idx) { return input1.dimension(idx); });
+
+  std::transform(right_idx.begin(), right_idx.end(),
+                 result_dimensions.begin() + left_idx.size(),
+                 [&](size_t idx) { return input2.dimension(idx); });
   result.resize(result_dimensions);
 
-  Eigen::ThreadPool pool(12 /* number of threads in pool */);
-
-  Eigen::ThreadPoolDevice my_device(&pool, 12 /* number of threads to use */);
+  Eigen::ThreadPool pool(12);
+  Eigen::ThreadPoolDevice my_device(&pool, 12);
   result.device(my_device) = input1.contract(input2, contract_dims);
 
   if (shuffle_idx.empty()) {
     return result;
   } else {
     Eigen::array<int, ResultDim> shuffle_array;
-    for (int i = 0; i < ResultDim; ++i) {
-      shuffle_array[i] = shuffle_idx[i];
-    }
+    std::copy(shuffle_idx.begin(), shuffle_idx.end(), shuffle_array.begin());
     return result.shuffle(shuffle_array);
   }
 }
+
+#endif
