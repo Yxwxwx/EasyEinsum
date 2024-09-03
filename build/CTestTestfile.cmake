fda70cd5756d95c406c8cbbcf4dd1ad836486cb8
Sun Xinyu <sunxinyu347@gmail.com>
2024-09-03 16:23:29 +0800

    Just for test

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yx/code/EasyEinsum
-# Build directory: /home/yx/code/EasyEinsum/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/yx/code/EasyEinsum/build/Einsum[1]_include.cmake")
-subdirs("_deps/googletest-build")
-subdirs("_deps/fmt-build")
--- build/Einsum[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/yx/code/EasyEinsum/build/Einsum[1]_tests.cmake")
-  include("/home/yx/code/EasyEinsum/build/Einsum[1]_tests.cmake")
-else()
-  add_test(Einsum_NOT_BUILT Einsum_NOT_BUILT)
-endif()
